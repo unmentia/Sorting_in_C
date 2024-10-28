dea4f58dafa9b45692fd6ab5720d023e0510ead4
jorabek <turnerko>
2024-10-28 12:03:32 +0500

    sorting

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    int arr[10] = {4, 6, 1, 67, 2, 456, 3, 0 , 24, 1};
-    for(int i=0; i<10; i++){
-        for(int j=0; j<10-1; j++){
+    int arr[10] = {4, 6, 1, 67, 2, 456, 3, 0 , 24, 1, 2};
+    for(int i=0; i<11; i++){
+        for(int j=0; j<11-1; j++){
             if(arr[j]>arr[j+1]){
                 int t=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=t;
             }
         }
-    } for(int i=0; i<10; i++){
+    } for(int i=0; i<11; i++){
         printf("%d ", arr[i]);
     } return 0;
 }
